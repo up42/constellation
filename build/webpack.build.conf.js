89b35f9ab54caed1244941bb7b459ddb99098c45
Matteo Pescarin <matteo.pescarin@up42.com>
2019-11-01 09:57:23 +0100

    added compiled dist files
    - added webpack config
    - included dist files to git
    - more webpack dependencies

---
+++ build/webpack.build.conf.js
@@ -0,0 +1,64 @@
+const config = require("../constellation.config");
+const webpack = require("webpack");
+const OptimizeCSSPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const webpackConfig = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: ["./src/styles/marketing.scss", "./src/styles/base.scss"],
+  output: {
+    path: config.output.base
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "css/[name].css"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "css-loader?-url"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new OptimizeCSSPlugin({
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default", { discardComments: { removeAll: true } }]
+      }
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: config.assets,
+        to: config.output.assets,
+        ignore: [".*"]
+      },
+      {
+        from: config.styles,
+        to: config.output.styles
+      },
+      {
+        from: `${config.components}/*.vue`,
+        to: config.output.components
+      }
+    ])
+  ]
+};
+
+module.exports = webpackConfig;
